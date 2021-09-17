5905dd7dbd59c649a930e48da7586d7ca4c4be58
mehhrnaz-ghaffar <mehrnaz.ghaffar@gmail.com>
2021-09-17 19:22:38 +0430

    made some change

--- src/view/pages/Home/index.vue
+++ src/view/pages/Home/index.vue
@@ -6,11 +6,12 @@
     >
     <div id="introduction">
       <h1>Event Calendar</h1>
+      <br>
       <h2>How to install :</h2>
       <p>Add this to your package's pubspec.yaml file:</p>
     </div>
 
-    <div class="code">
+    <div class="code" id="long-div">
       <pre class="mb-0 py-2">dependencies: 
     flutter_event_calendar: ^0.0.8 </pre>
     </div>
@@ -24,13 +25,16 @@
     </div>
     <p>Alternatively, your editor might support flutter pub get. Check the docs for your editor to learn more.
       Now in your Dart code, you can use:</p>
-    <div class="code">
+    <div class="code" id="long-div">
       <p class="mb-0 py-2">import 'package:flutter_event_calendar/flutter_event_calendar.dart';</p>
     </div>
-    <!-- <code>mdden dnwndje</code>
-    <code>mdden neinfjorn</code>
-    <code>mdden mjfineif</code>
-    <code>mdden mfdfjn</code>  -->
+    <!-- <code> this is a line of code </code>
+    <code>> this is a line of code </code>
+    <code>> this is a line of code </code>
+    <code>> this is a line of code </code>  -->
+    <img
+     src="../../../assets/images/event_calendar_options.png"
+     alt="">
     
   </div>
 </template>
@@ -45,14 +49,35 @@
     margin-top: 50px;
   }
 
+  h1{
+    font-size: 35px;
+    border-bottom: 1px solid rgb(206, 206, 216);
+    padding-bottom: 3px;
+  }
+
+  h2{
+    font-size: 27px;
+    border-bottom: 1px solid rgb(206, 206, 216);
+    padding-bottom: 3px;
+  }
+
   .code{
     font-size: 15px;
     background-color: #f3f3f3;
-    box-shadow: 4px 7px 1px #cfcfcf;
+    box-shadow: 3px 3px 6px #e1e1e1;
     border-radius: 2px;
     padding: 10px;
     font-family: monospace;
     margin: 20px 0;
   }
-  
+
+  p{
+    margin-top: 16px;
+  }
+
+  @media only screen and (max-width : 820px) {
+    #long-div{
+      width : fit-content;
+    }
+  }
 </style>
