0b1b36cc0b79ada4cf66e0d32924506466ddb60a
mehhrnaz-ghaffar <mehrnaz.ghaffar@gmail.com>
2021-09-19 09:24:53 +0430

    added some content

--- src/view/pages/Basics/index.vue
+++ src/view/pages/Basics/index.vue
@@ -3,12 +3,64 @@
     <h1>
       this is Basics content
     </h1>
+    <v-container>
+      <v-radio-goup v-model="radioGroup">
+        <v-radio 
+          v-for= "item in array"
+          :key="item.name"
+          :label="`Radio ${item.name}`"
+        >
+        </v-radio>
+        <v-container>
+          <img 
+            v-if="RadioGroup===0"
+            :src="img1" 
+            alt=""
+          >
+          <img 
+            v-if="RadioGroup===1"
+            :src="img2"
+            alt="">
+          <img 
+            v-if="RadioGroup===2"
+            :src="img1"
+            alt="">
+        </v-container>
+      </v-radio-goup>
+      <v-switch
+        v-model="switch1"
+        :lablel="`Switch 1: ${switch1.toString()}`"
+        ></v-switch>
+
+      <img 
+      v-if="switch1"
+      :src="img1" 
+      alt="">
+      <img 
+      v-else
+      :src="img2" 
+      alt="">
+
+    </v-container>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Basics'
+    name: 'Basics',
+    data() {
+      return {
+        array: [
+          {name: 'item1'},
+          {name: 'item2'},
+          {name: 'item3'},
+        ],
+        img1: "../../../assets/images/screenshout1.jpg",
+        img2: "../../../assets/images/screenshout2.jpg",
+        RadioGroup :0,
+        switch1: true
+      }
+    },
   }
 </script>
 
--- src/view/pages/Calendar/index.vue
+++ src/view/pages/Calendar/index.vue
@@ -3,17 +3,77 @@
     <h1>
       this is Calendar content
     </h1>
+    <v-container>
+      <v-radio-group v-model="radioGroup">
+        <v-radio
+          v-for="item in array"
+          :key="item.name"
+          :label="`Radio ${item.name}`"
+        ></v-radio>
+        <v-container>
+          <img
+            v-if="radioGroup === 0"
+            src="../../../assets/images/screenshout1.jpg"
+            alt="event_calendar_options"
+            class="calendar_image-width"
+          >
+          <img
+            v-if="radioGroup === 1"
+            src="../../../assets/images/screenshout2.jpg"
+            alt="event_calendar_options"
+            class="calendar_image-width"
+          >
+          <img
+            v-if="radioGroup === 2"
+            src="../../../assets/images/screenshout1.jpg"
+            alt="event_calendar_options"
+            class="calendar_image-width"
+          >
+        </v-container>
+      </v-radio-group>
+      <v-switch
+        v-model="switch1"
+        :label="`Switch 1: ${switch1.toString()}`"
+      ></v-switch>
+      <img
+        v-if="switch1"
+        src="../../../assets/images/screenshout1.jpg"
+        alt="event_calendar_options"
+        class="calendar_image-width"
+      >
+      <img
+        v-else
+        src="../../../assets/images/screenshout2.jpg"
+        alt="event_calendar_options"
+        class="calendar_image-width"
+      >
+    </v-container>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Calendar'
+    name: 'Contributing',
+    data() {
+      return {
+        switch1: true,
+        array: [
+          {name: 'Issue 1'},
+          {name: 'Issue 2'},
+          {name: 'Issue 3'}
+        ],
+        radioGroup: 0
+      }
+    },
   }
 </script>
 
 <style>
   .Home {
     padding: 50px;
+  }
+
+  .calendar_image-width {
+    max-width: 100%;
   }
 </style>
